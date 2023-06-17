83bd66eb082610fc2de79d522ffa6f064012119e
citadin <citadinrodrigo@gmail.com>
2023-06-17 02:02:32 -0300

    resolving some bugs...

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,19 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "dummy",
+      password: "dummy",
+      database: "nest_graphql_api",
+      entities: [
+        "dist/**/*.entity{.ts,.js}"
+      ],
+      synchronize: true,
+    }
+    ),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
