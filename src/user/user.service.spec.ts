c307b0910bcf5728c97b95f379a98e07190816d4
citadin <citadinrodrigo@gmail.com>
2023-07-10 21:14:04 -0300

    test: error in creating a new user

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -3,7 +3,7 @@ import { UserService } from './user.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import TestUtils from '../common/test/TestUtils';
-import { NotFoundException } from '@nestjs/common';
+import { InternalServerErrorException, NotFoundException } from '@nestjs/common';
 
 describe('UserService', () => {
   let service: UserService;
@@ -66,10 +66,12 @@ describe('UserService', () => {
       expect(mockRepository.findOne).toHaveBeenCalledTimes(1);
     })
 
-    it('should return an exception when does not find an user', () => {
+    it('should return an exception when does not find an user', async () => {
       mockRepository.findOne.mockReturnValue(null);
 
-      expect(service.findUserById('1')).rejects.toBeInstanceOf(NotFoundException);
+      await service.findUserById('1').catch(e => {
+        expect(e).toBeInstanceOf(NotFoundException);
+      })
       expect(mockRepository.findOne).toHaveBeenCalledTimes(1);
     })
   })
@@ -80,6 +82,17 @@ describe('UserService', () => {
       mockRepository.create.mockReturnValue(validUser);
 
       expect(await service.createUser(validUser)).toMatchObject(validUser);
+      expect(mockRepository.create).toBeCalledTimes(1);
+      expect(mockRepository.save).toBeCalledTimes(1);
+    })
+
+    it('should return an exception when doesnt create an user', async () => {
+      mockRepository.save.mockReturnValue(null);
+      mockRepository.create.mockReturnValue(validUser);
+
+      await service.createUser(validUser).catch(e => {
+        expect(e).toBeInstanceOf(InternalServerErrorException);
+      })
       expect(mockRepository.create).toBeCalledTimes(1);
       expect(mockRepository.save).toBeCalledTimes(1);
     })
