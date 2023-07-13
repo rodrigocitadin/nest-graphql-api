183e088baaf2bb441f06f3946545a462417692af
citadin <citadinrodrigo@gmail.com>
2023-07-13 19:17:01 -0300

    feat: login user

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,4 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthInput } from './dto/auth.input';
+import { AuthType } from './dto/auth.type';
+import { AuthService } from './auth.service';
 
 @Resolver()
-export class AuthResolver {}
+export class AuthResolver {
+  constructor(private authService: AuthService) { }
+
+  @Mutation()
+  public async login(
+    @Args('data') data: AuthInput
+  ): Promise<AuthType> {
+    const response = await this.authService.validateUser(data);
+
+    return {
+      user: response.user,
+      token: response.token,
+    }
+  }
+}
