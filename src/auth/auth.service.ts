28d33b7e9a27b95b6dd72eb80a4f036ec1d93648
citadin <citadinrodrigo@gmail.com>
2023-07-13 19:04:26 -0300

    feat: auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { AuthInput } from './dto/auth.input';
+import { compareSync } from 'bcrypt';
+import { AuthType } from './dto/auth.type';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) { }
+
+  async validateUser(data: AuthInput): Promise<AuthType> {
+    const user = await this.userService.findUserByEmail(data.email);
+
+    const validPassword = compareSync(data.password, user.password);
+
+    if (!validPassword) {
+      throw new UnauthorizedException('Incorrect Password');
+    }
+
+    return {
+      user, token: 'token'
+    }
+  }
+}
